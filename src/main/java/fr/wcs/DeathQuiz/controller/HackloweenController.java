de2b12ce4c0216307adf4f886185fb35dbcfc680
soninlaw <jonsoninlaw@gmail.com>
2019-10-30 23:22:14 +0100

    Ajout de la vérification de la réponse

--- src/main/java/fr/wcs/DeathQuiz/controller/HackloweenController.java
+++ src/main/java/fr/wcs/DeathQuiz/controller/HackloweenController.java
@@ -6,6 +6,7 @@ import fr.wcs.DeathQuiz.model.Questions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -15,7 +16,7 @@ import java.io.IOException;
 @Controller
 public class HackloweenController {
 
-    //private static Questions questions = new Questions();
+    private static Questions questions = new Questions();
 
     private static final String HACKLOWEEN_URL = "https://hackathon-wild-hackoween.herokuapp.com/";
 
@@ -24,13 +25,27 @@ public class HackloweenController {
         return "index";
     }
 
-    @GetMapping("/question1")
-    public String question1(Model out) {
-        Questions questions = new Questions();
-        out.addAttribute("question", questions.getMovie1());
-        return "question";
-        //return questions.getMovie1().getTitle() + " ------- " + questions.getMovie1().getQuestion() + " ------- " + questions.getMovie1().getAnswers() + " --- The right answer is : " + questions.getMovie1().getRightAnswers();
+    @GetMapping("/question")
+    public String question(Model out, @RequestParam(name = "answer", required = false, defaultValue = "") String answer) {
 
+        String right = "none";
+        String wrong = "none";
+        Object rightAnswer = questions.getMovie1().getRightAnswers() + "";
+        System.out.println(rightAnswer);
+        System.out.println(answer);
+        if (answer.length() != 0) {
+            if (rightAnswer.equals(answer)) {
+                right = "block";
+            }
+            else {
+                wrong = "block";
+            }
+        }
+
+        out.addAttribute("question", questions.getMovie1());
+        out.addAttribute("right", right);
+        out.addAttribute("wrong", wrong);
+        return "question";
     }
 
 }
