017fec5dd9f6118da63dae47bd48b8293c43f3f8
soninlaw <jonsoninlaw@gmail.com>
2019-10-30 15:05:50 +0100

    Initialisation du projet Springboot

---
+++ src/test/java/fr/wcs/DeathQuiz/DeathQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wcs.DeathQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
